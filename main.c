4b2f3e2d961be01a19d35fc35d3a2d621d582b32
Kumatetsu <billaujean@gmail.com>
2018-05-22 10:58:54 -0400

    basic main to wich open a window and wait quit action

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL/SDL_image.h>
+
+int main (int argc, char *argv[])
+{
+  int 	     quit = 0;
+  SDL_Window *window;
+  SDL_Event  event;
+
+  //init sdl
+  SDL_Init(SDL_INIT_VIDEO);
+
+  //init the window
+  window = SDL_CreateWindow(
+			    "Bomberman",
+			    SDL_WINDOWPOS_CENTERED,
+			    SDL_WINDOWPOS_CENTERED,
+			    800,
+			    600,
+			    SDL_WINDOW_SHOWN
+			    );
+  if (window == NULL) {
+    printf("windows not created");
+    return 1;
+  }
+
+  while(!quit) {
+    SDL_WaitEvent(&event);
+
+    switch(event.type){
+    case SDL_QUIT:
+      quit = 1;
+      break;
+    }
+  }
+  
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
