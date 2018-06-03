503bfd375fca0faf0a25df27ffd56579b6177a88
Kumatetsu <billau_j@etna-alternance.net>
2018-06-03 14:32:32 +0200

    creation d'un socket server

--- server/server.c
+++ server/server.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include  <stdio.h>
+#include  <string.h>
+#include  <sys/types.h>
+#include <unistd.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include "sdl.h"
 #include "client.h"
+#include "server.h"
 
 void		init_server(t_sdl *sdl) {
-  //launch server thread
-  client_loop(sdl);
-  //kill thread
+  t_server	*s;
+  pid_t		pid;
+
+  s = create_server();
+  init_socket(&s);
+
+
   return;
 }
---
+++ server/socket.c
@@ -0,0 +1,29 @@
+#include <sys/socket.h>
+#include <netdb.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include "server.h"
+
+void 			init_socket(t_server **s) {
+
+  int			socket_fd;
+  struct protoent	*pe;
+  struct sockaddr_in	sin;
+  
+  pe = getprotobyname("tcp");
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, pe->p_proto)) == -1)
+    return;
+
+  sin.sin_family = AF_INET;
+  sin.sin_port = htons(PORT);
+  sin.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(socket_fd, (struct sockaddr*)&sin, sizeof (sin)) == -1)
+    return;
+
+  if (listen(socket_fd, 42) == -1)
+    return;
+
+  (*s)->server_fd = socket_fd;
+}
