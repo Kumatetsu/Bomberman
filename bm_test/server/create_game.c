6f61852749e442d82ee20f9cad840b8e7b9a1291
Nicolas Hochart <ni.hochart@gmail.com>
2018-07-01 17:10:54 +0200

    add game creation

--- bm_test/server/create_game.c
+++ bm_test/server/create_game.c
@@ -7,18 +7,21 @@
 #include "game_info.h"
 #include "my_put.h"
 
-t_game_info *create_game_info(int fd1, int fd2, int fd3, int fd4) {
+t_game_info *create_game_info(int fd1, int fd2, int fd3, int fd4)
+{
     t_game_info *game_info;
     t_map_destroyable *map_destroyable;
     int i;
 
     game_info = calloc(1, sizeof(t_game_info));
-    for (i = 0; i < 3; ++i) {
+    for (i = 0; i < 3; ++i)
+    {
         game_info->players[i] = calloc(1, sizeof(t_player_info));
         game_info->players[i]->connected = 1;
         game_info->players[i]->bomb_left = 1;
         game_info->players[i]->num_player = i + 1;
-        switch (i) {
+        switch (i)
+        {
             case 0:
                 game_info->players[i]->fd = fd1;
                 game_info->players[i]->x_pos = 2;
@@ -45,7 +48,7 @@ t_game_info *create_game_info(int fd1, int fd2, int fd3, int fd4) {
                 break;
         }
     }
-    for (i=0; i < 32; ++i)
+    for (i = 0; i < 32; ++i)
     {
         map_destroyable = calloc(1, sizeof(t_map_destroyable));
         map_destroyable->wall_destroyable = 1;
@@ -57,21 +60,17 @@ t_game_info *create_game_info(int fd1, int fd2, int fd3, int fd4) {
     return game_info;
 }
 
-long random_at_most(long max) {
-    unsigned long
-    // max <= RAND_MAX < ULONG_MAX, so this is okay.
-            num_bins = (unsigned long) max + 1,
-            num_rand = (unsigned long) RAND_MAX + 1,
-            bin_size = num_rand / num_bins,
-            defect   = num_rand % num_bins;
+long random_at_most(long max)
+{
+    unsigned long num_bins = (unsigned long) max + 1;
+    unsigned long num_rand = (unsigned long) RAND_MAX + 1;
+    unsigned long bin_size = num_rand / num_bins;
+    unsigned long defect = num_rand % num_bins;
 
     long x;
     do {
         x = random();
-    }
-        // This is carefully written not to overflow
-    while (num_rand - defect <= (unsigned long)x);
+    } while (num_rand - defect <= (unsigned long) x);
 
-    // Truncated division is intentional
-    return x/bin_size;
+    return x / bin_size;
 }
