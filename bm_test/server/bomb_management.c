dc271b9fa8c7f1e05d056acfeb1ca537d8c222c6
Nicolas Hochart <ni.hochart@gamil.com>
2018-07-01 22:28:53 +0200

    more norme, promis je vais faire autre chose !

--- bm_test/server/bomb_management.c
+++ bm_test/server/bomb_management.c
@@ -5,7 +5,7 @@
 ** Login   <billau_j@etna-alternance.net>
 **
 ** Started on  Tue Jun 26 17:26:19 2018 BILLAUD Jean
-** Last update Sun Jul  1 22:10:22 2018 hochar_n
+** Last update Sun Jul  1 22:26:31 2018 hochar_n
 */
 
 #include <stdio.h>
@@ -35,42 +35,42 @@ void			trigger_bomb(
       pos = (i == 0 ? bomb->x_pos : bomb->y_pos);
       for (i = pos; i < pos + 8; ++i)
 	{
-        apply_bomb_to_position(bomb, map_pointer, game_info);
+	  apply_bomb_to_position(bomb, map_pointer, game_info);
 	}
     }
 }
 
-void    apply_bomb_to_position(t_map_destroyable *bomb,
-                               int **map_pointer,
-                               t_game_info *game_info
-                                )
+void    		apply_bomb_to_position(t_map_destroyable *bomb,
+					       int **map_pointer,
+					       t_game_info *game_info
+					       )
 {
-    int k;
-    int is_blocked;
-    int x;
-    int y;
-    int current_pos;
-    t_map_destroyable	*presence;
+  int 			k;
+  int 			is_blocked;
+  int 			x;
+  int 			y;
+  int 			current_pos;
+  t_map_destroyable	*presence;
 
-    is_blocked = 0;
-    current_pos = i == 0 ? bomb->y_pos : bomb->x_pos;
-    for (k = (current_pos-3); k < (current_pos+3); ++k) {
-        if (k == 0 || is_blocked == 1)
-        {
-            is_blocked = (k == 0 ? 0 : 1);
-            continue;
-        }
-        x = i == 0 ? k : pos;
-        y = i == 0 ? pos : k;
-        if (map_pointer[x][y] == WALL)
-        {
-            is_blocked = 1;
-            continue;
-        }
-        map_pointer[x][y] = FIRE;
-        presence = get_element_at_pos(game_info, x, y);
-        apply_explosion(presence, bomb, game_info, map_pointer);
-    }
+  is_blocked = 0;
+  current_pos = i == 0 ? bomb->y_pos : bomb->x_pos;
+  for (k = (current_pos-3); k < (current_pos+3); ++k) {
+    if (k == 0 || is_blocked == 1)
+      {
+	is_blocked = (k == 0 ? 0 : 1);
+	continue;
+      }
+    x = i == 0 ? k : pos;
+    y = i == 0 ? pos : k;
+    if (map_pointer[x][y] == WALL)
+      {
+	is_blocked = 1;
+	continue;
+      }
+    map_pointer[x][y] = FIRE;
+    presence = get_element_at_pos(game_info, x, y);
+    apply_explosion(presence, bomb, game_info, map_pointer);
+  }
 }
 
 void			destroy_bomb(
@@ -151,25 +151,31 @@ t_map_destroyable	*get_element_at_pos(t_game_info *game_info, int x, int y)
     for (j = 1; j < 15; ++j) {
       if (game_info->map_destroyable[i][j] == NULL
 	  || game_info->map_destroyable[i][j]->bomb == 1)
-          continue;
+	continue;
       map_destroyable = game_info->map_destroyable[i][j];
       if (map_destroyable->y_pos == y && map_destroyable->x_pos == x)
-          return map_destroyable;
+	return map_destroyable;
     }
   }
   return NULL;
 }
 
-int apply_explosion(t_map_destroyable *element, t_map_destroyable *bomb, t_game_info *game_info, int **map_pointer) {
-    if (presence != NULL && presence->bomb == 0)
-        presence->dying++;
-    else if (presence != NULL && presence->bomb == 1
-             && bomb->start_explode > game_info->tick_time)
+int apply_explosion(
+		    t_map_destroyable *element,
+		    t_map_destroyable *bomb,
+		    t_game_info *game_info,
+		    int **map_pointer
+		    )
+{
+  if (presence != NULL && presence->bomb == 0)
+    presence->dying++;
+  else if (presence != NULL && presence->bomb == 1
+	   && bomb->start_explode > game_info->tick_time)
     {
-        bomb->start_explode = game_info->tick_time;
-        trigger_bomb(game_info, map_pointer, presence);
+      bomb->start_explode = game_info->tick_time;
+      trigger_bomb(game_info, map_pointer, presence);
     }
-    else
-        return 0;
-    return 1;
+  else
+    return 0;
+  return 1;
 }
