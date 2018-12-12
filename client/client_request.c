2bd0a2b3ec2680a7552227fc6207c5b5705f1228
Kumatetsu <billau_j@etna-alternance.net>
2018-12-12 17:47:38 +0100

    correction request
    + placement des joueurs en pixels

--- client/client_request.c
+++ client/client_request.c
@@ -48,11 +48,11 @@ int free_player_request(t_player_request* client_request)
 int         send_request(int s, t_player_request* client_request)
 {
   // char*     request_string;
-  t_player_request dum_static;
+  t_player_request dumb_static;
   // request_string = request_serialization(client_request);
   memcpy(&dumb_static.checksum, &client_request->checksum, sizeof(int));
   memcpy(&dumb_static.magic, &client_request->magic, sizeof(int));
-  memcpy(&dumb_static.command, &client_request->client_request, sizeof(int));
+  memcpy(&dumb_static.command, &client_request->command, sizeof(int));
 
   write(s, &dumb_static, sizeof(t_player_request));
   return SUCCESS_SEND;
