eea52a80146ff2f3d5cf7ed9a62545932061f01d
Kumatetsu <billau_j@etna-alternance.net>
2018-12-29 11:44:59 +0100

    Inversion de fonction pour afficher le joueur

--- client/client_receive.c
+++ client/client_receive.c
@@ -10,7 +10,9 @@
 
 #include <pthread.h>
 #include "enum.h"
+#include "constant.h"
 #include "sdl.h"
+#include "bomber_sprites.h"
 #include "client.h"
 #include "player_info.h"
 #include "client_request.h"
@@ -21,24 +23,29 @@
 #include "game_info_serialization.h"
 #include "data.h"
 #include "thread.h"
+#include "draw_base_map.h"
 #include "base_map_manager.h"
 #include "move_player.h"
 #include "client_receive.h"
 
 void            *listen_server(void *s)
 {
+  int		i;
   t_thread	*struct_thread;
   int           quit = 0;
   fd_set        fd_read;
+  t_game_info	*game_info;
+  t_data	*data;
 
   struct_thread= (t_thread *)(s);
+  data = struct_thread->data;
   while (!quit)
     {
       FD_ZERO(&fd_read);
       FD_SET(struct_thread->socket, &fd_read);
       printf("\nbefore select\n");
       if (select((struct_thread->socket + 1), &fd_read, NULL, NULL, NULL) == -1)
-              quit = 1;
+	quit = 1;
       if (FD_ISSET(struct_thread->socket, &fd_read))
         {
 	  if (get_message(struct_thread->socket) == 0)
@@ -47,10 +54,11 @@ void            *listen_server(void *s)
 	  for (i = 0; i < INLINE_MATRIX; i++)
 	    data->map_destroyable[i] = game_info->map_destroyable[i];
 	  SDL_RenderClear(data->renderer);
-	  // redéfini le model de la map fixe dans array_map
-	  draw_all(data);
 	  // rebuild le model dans array_map
 	  rebuild_map(data);
+
+    // redéfini le model de la map fixe dans array_map
+	  draw_all(data);
 
 	  // fonction cheloue:
 	  // move_player_stop(data);
@@ -59,14 +67,6 @@ void            *listen_server(void *s)
 	  // rempli le model dans map_destroyable
 	  draw_destroyable_model(data);
 	  // appel SDL_RenderCopy sur map_destroyable
-    if (game_info != NULL)
-    {
-      for (i = 0; i < 4; i++)
-	{
-	  if (game_info->players[i].connected && game_info->players[i].alive)
-	    draw_player(data, game_info->players[i]);
-	}
-    }
 	  build_destroyables(data);
 	  // dessine le contenu du renderer dans la window
 	  SDL_RenderPresent(data->renderer);
@@ -76,26 +76,24 @@ void            *listen_server(void *s)
   pthread_exit(NULL);
 }
 
-int get_message(int s)
+int		get_message(int s)
 {
-  char buff[sizeof(t_game_info)];
-  int r;
-  t_game_info *game_info;
-  int i;
+  int		r;
+  char		buff[sizeof(t_game_info) + 1];
+  t_game_info	*game_info;
 
   game_info = NULL;
-  r = recv(s, buff, sizeof(t_game_info) - 1 , 0);
-  if (r > 0)
+  r = recv(s, buff, sizeof(t_game_info) + 1, 0);
+  // une t_game_info fait plus de 7000 bytes
+  // si l'ensemble n'est pas consommé, il peut rester
+  // quelques bytes qui seront traités et provoqueront une erreur
+  // ici, un système permettant de checké l'intégrité de la
+  // game_info serait le bienvenu, c'était la cause du bug de sérialisation
+  if (r > 3000)
   {
-    //deserialize_game_info(buff);
     game_info = (t_game_info*)buff;
     set_game_info(game_info);
-    //game_info = get_game_info();
-    printf("tick_time %d \n", game_info->tick_time);
-    for (i=0; i<4; i++) {
-      //      if (&game_info->players[i])
-	//      printf("client fd %d\n\n", (int)game_info->players[i].fd);
-    }
+    printf("tick_time %d received %d bytes\n", game_info->tick_time, r);
     return 1;
   }
   else
